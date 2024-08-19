bd31dd07f344f76c79850e88a0d3c9b32a2119a8
elixez9 <a1381ahmadi@gmail.com>
2024-08-20 02:46:16 +0430

    coment

--- bot.py/bot.py
+++ bot.py/bot.py
@@ -6,6 +6,8 @@ while True:
     w=input("enter your messege:")
     if w == "end":
         break
+    ##makskone
     time.sleep(5)
     for i in range(10):
         pyautogui.press("enter")
+        
